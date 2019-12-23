b139c0fefb962d1fb10e0f62d31569f2836e6b78
Vincent Le <vince1123581321@gmail.com>
2019-12-23 10:28:53 -0800

    Day 13.

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::collections::VecDeque;
 
 use crate::intcode_computer::{self, Computer};
@@ -8,22 +9,93 @@ fn parse(input: &str) -> Vec<i64> {
 }
 
 #[aoc(day13, part1)]
-fn part1(program: &[i64]) -> i64 {
+fn part1(program: &[i64]) -> usize {
     let mut computer = Computer::initialize(program);
     let mut input = VecDeque::new();
     let mut output = VecDeque::new();
 
     computer.run_with_io(&mut input, &mut output);
-    println!("{:?}", output);
-    output.iter().fold(
-        (0, 0), 
-        |acc, tile_id| {
-            let triplet_index = (acc.0 + 1) % 3;
-            if triplet_index == 0 && *tile_id == 2 {
-                (triplet_index, acc.1 + 1)
-            } else {
-                (triplet_index, acc.1)
-            }
+    let (tiles, _) = read_output(&mut output);
+    tiles.iter().filter(|t| t.kind == TileKind::Block).count()
+}
+
+#[derive(PartialEq)]
+enum TileKind {
+    Empty, Wall, Block, Paddle, Ball
+}
+
+#[allow(dead_code)]
+struct Tile {
+    x: i64,
+    y: i64,
+    kind: TileKind,
+}
+
+impl Tile {
+    fn parse(triple: &[i64]) -> Tile {
+        let kind = match triple[2] {
+            0 => TileKind::Empty,
+            1 => TileKind::Wall,
+            2 => TileKind::Block,
+            3 => TileKind::Paddle,
+            4 => TileKind::Ball,
+            _ => panic!(),
+        };
+
+        Tile {
+            x: triple[0],
+            y: triple[1],
+            kind: kind,
         }
-    ).1
+    }
+}
+
+fn read_output(output: &mut VecDeque<i64>) -> (Vec<Tile>, i64) {
+    let mut tiles = vec![];
+    let mut score = 0;
+    while output.len() >= 3 {
+        let triple: Vec<i64> = output.drain(0..3).collect();
+        if triple[0] == -1 && triple[1] == 0 {
+            score = triple[2];
+        } else {
+            tiles.push(Tile::parse(&triple));
+        }
+    }
+    (tiles, score)
+}
+
+#[aoc(day13, part2)]
+fn part2(program: &[i64]) -> i64 {
+    let mut program = program.to_vec();
+    program[0] = 2;
+
+    beat_game(&program)
+}
+
+fn beat_game(program: &[i64]) -> i64 {
+    let mut ball_x = 0;
+    let mut paddle_x = 0;
+    let mut score = 0;
+
+    let mut computer = Computer::initialize(program);
+    let mut input = VecDeque::new();
+    let mut output = VecDeque::new();
+
+    while !computer.is_halted() {
+        computer.run_with_io(&mut input, &mut output);
+        let (tiles, latest_score) = read_output(&mut output);
+        score = latest_score;
+
+        ball_x = tiles.iter().filter(|t| t.kind == TileKind::Ball).fold(ball_x, |_acc, tile| tile.x);
+        paddle_x = tiles.iter().filter(|t| t.kind == TileKind::Paddle).fold(paddle_x, |_acc, tile| tile.x);
+        
+        let joystick_input = match ball_x.cmp(&paddle_x) {
+            Ordering::Less => -1,
+            Ordering::Equal => 0,
+            Ordering::Greater => 1,
+        };
+        input.push_back(joystick_input);
+    }
+
+    score
 }
